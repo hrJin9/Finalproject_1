9184d52e3a9d605a847f2ed6b5a9fb19b94f8eee
yenioo <yelee0124@gmail.com>
2022-12-09 17:20:41 +0900

    예은 중간 푸시

--- FinalProject_1/src/main/java/com/spring/yeeun/model/AttendanceDAO.java
+++ FinalProject_1/src/main/java/com/spring/yeeun/model/AttendanceDAO.java
@@ -20,7 +20,49 @@ public class AttendanceDAO implements InterAttendanceDAO {
 		int n = sqlsession.insert("attendance.addAttendance", paraMap);
 		return 0;
 	}
+	
+	// 총 근무시간 조회하기
+	/*
+	@Override
+	public Map<String, String> getTotalTime(Map<String, String> paraMap) {
+		Map<String, String> totalTime = sqlsession.selectOne("attendance.getTotalTime",paraMap);
+		return totalTime;
+	}
+	*/
+	
+	// 저장한 근무상태 조회하기
+	@Override
+	public List<AttendanceVO> atdetailView(Map<String, String> paraMap) {
+		List<AttendanceVO> attendancedetail = sqlsession.selectList("attendance.atdetailView", paraMap);
+		return attendancedetail;
+	}
+		
+	// 저장한 근무상태 한개 삭제하기
+	@Override
+	public int atdetailDel(String adno) {
+		int n = sqlsession.delete("attendance.atdetailDel", adno);
+		return n;
+	}		
+	
+	// 근무상태 전부 삭제하기
+	@Override
+	public int atdetailAllDel(Map<String, String> paraMap) {
+		int n = sqlsession.delete("attendance.atdetailAllDel", paraMap);
+		return n;
+	}	
 
+	// 전체 근무목록 보여주기 
+	@Override
+	public List<AttendanceVO> getworkTimebyDay(Map<String, String> paraMap) {
+		List<AttendanceVO> workTimeList = sqlsession.selectList("attendance.getworkTimebyDay", paraMap);
+		return workTimeList;
+	}
+	
+	
+	
+	
+	
+	
 	
 	// 휴가 개요 보여주기
 	@Override
@@ -29,7 +71,6 @@ public class AttendanceDAO implements InterAttendanceDAO {
 		return dayoffList;
 	}
 
-
 	// 년도별 휴가 개용 보여주기
 	@Override
 	public List<DayoffVO> dayoffListViewByYear(Map<String, String> paraMap) {
@@ -37,13 +78,30 @@ public class AttendanceDAO implements InterAttendanceDAO {
 		return dayoffListbyYear;
 	}
 
-
 	// 매월 연차 +1 스프링 스케줄러 ==> 수정필요
 	@Override
 	public List<DayoffVO> dayoffAddScheduler(DayoffVO dayoffvo) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+
+
+
+	
+	
+
+
+
+
+
+
+
+
+	
+
+
+	
 
 	
 
