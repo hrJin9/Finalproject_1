c976866c501405ad5253495b1d10d2cd9474e7f8
yenioo <yelee0124@gmail.com>
2022-12-09 15:26:17 +0900

    예은 중간푸시 - 사이드바 수정

--- FinalProject_1/src/main/java/com/spring/yeeun/model/AttendanceVO.java
+++ FinalProject_1/src/main/java/com/spring/yeeun/model/AttendanceVO.java
@@ -2,21 +2,31 @@ package com.spring.yeeun.model;
 
 public class AttendanceVO {
 
-	private String adno;         // 근태번호
+	private String adno;            // 근태번호
 	private String fk_employee_no;  // 사원번호
-	private String adcatgo;      // 근태종류
-	private String startdate;    // 시작시간
-	private String enddate;      // 종료시간
+	private String adcatgo;         // 근태유형
+	private String startTime;       // 시작시간
+	private String endTime;         // 종료시간
+	
+	private String seldate;   // 선택한 날짜
+	private String workTime;  // 근무 시간
+	private String workMin;   // 근무 분
+	private String totalTime; // 총 근무시간
 	
 	public AttendanceVO() {}
 
-	public AttendanceVO(String adno, String fk_employee_no, String adcatgo, String startdate, String enddate) {
+	public AttendanceVO(String adno, String fk_employee_no, String adcatgo, String startTime, String endTime,
+			String seldate, String workTime, String workMin, String totalTime) {
 		super();
 		this.adno = adno;
 		this.fk_employee_no = fk_employee_no;
 		this.adcatgo = adcatgo;
-		this.startdate = startdate;
-		this.enddate = enddate;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.seldate = seldate;
+		this.workTime = workTime;
+		this.workMin = workMin;
+		this.totalTime = totalTime;
 	}
 
 	public String getAdno() {
@@ -43,21 +53,57 @@ public class AttendanceVO {
 		this.adcatgo = adcatgo;
 	}
 
-	public String getStartdate() {
-		return startdate;
+	public String getStartTime() {
+		return startTime;
 	}
 
-	public void setStartdate(String startdate) {
-		this.startdate = startdate;
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
 	}
 
-	public String getEnddate() {
-		return enddate;
+	public String getEndTime() {
+		return endTime;
 	}
 
-	public void setEnddate(String enddate) {
-		this.enddate = enddate;
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
 	}
+
+	public String getSeldate() {
+		return seldate;
+	}
+
+	public void setSeldate(String seldate) {
+		this.seldate = seldate;
+	}
+
+	public String getWorkTime() {
+		return workTime;
+	}
+
+	public void setWorkTime(String workTime) {
+		this.workTime = workTime;
+	}
+
+	public String getWorkMin() {
+		return workMin;
+	}
+
+	public void setWorkMin(String workMin) {
+		this.workMin = workMin;
+	}
+
+	public String getTotalTime() {
+		return totalTime;
+	}
+
+	public void setTotalTime(String totalTime) {
+		this.totalTime = totalTime;
+	}
+
+	
+	
+	
 	
 	
 	
